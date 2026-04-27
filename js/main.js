83ba66cbf35be8d5be6efcff588a119896045740
KSG44 <gon040422@naver.com>
2026-04-27 16:56:42 +0900

    refactor: 페이지 재배치

--- js/main.js
+++ js/main.js
@@ -5,11 +5,10 @@ import { LinkedList } from "./models/LinkedList.js";
 import { DoublyLinkedList } from "./models/DoublyLinkedList.js";
 import { CircularLinkedList } from "./models/CircularLinkedList.js";
 import { ArrayList } from "./models/ArrayList.js";
-import { HashTable } from "./models/HashTable.js";
 import { BST } from "./models/BST.js";
 import { MaxHeap } from "./models/Heap.js";
 import { AVLTree } from "./models/AVLTree.js";
-import { Graph } from "./models/Graph.js";
+// 그래프, 해시테이블 임포트 제거 완료
 
 const s = new Stack(),
   q = new Queue(),
@@ -21,187 +20,336 @@ const sl = new LinkedList(),
 const tr = new BST(),
   avl = new AVLTree(),
   hp = new MaxHeap();
-const ht = new HashTable();
-const g = new Graph();
-const nodePositions = {};
+
+// 리스트 폴백 로직
+const ensureListMethods = (list) => {
+  if (!list.clear)
+    list.clear = function () {
+      this.head = null;
+      this.size = 0;
+    };
+  if (!list.append)
+    list.append = function (val) {
+      this.addLast ? this.addLast(val) : this.tail ? this.addLast(val) : null;
+    };
+  if (!list.insertAt)
+    list.insertAt = function (idx, val) {
+      const arr = this.toArray();
+      arr.splice(idx, 0, val);
+      this.clear();
+      arr.forEach((v) => this.append(v));
+    };
+  if (!list.removeAt)
+    list.removeAt = function (idx) {
+      const arr = this.toArray();
+      arr.splice(idx, 1);
+      this.clear();
+      arr.forEach((v) => this.append(v));
+    };
+  if (!list.reverse)
+    list.reverse = function () {
+      const arr = this.toArray().reverse();
+      this.clear();
+      arr.forEach((v) => this.append(v));
+    };
+  if (!list.prepend)
+    list.prepend = function (val) {
+      const arr = this.toArray();
+      arr.unshift(val);
+      this.clear();
+      arr.forEach((v) => this.append(v));
+    };
+};
+ensureListMethods(sl);
+ensureListMethods(dl);
+ensureListMethods(cl);
 
 window.onload = () => {
-  // --- Sequential ---
-  if (document.getElementById("pushBtn")) {
-    document.getElementById("pushBtn").onclick = () => {
-      const i = document.getElementById("stackInput");
-      if (i.value) {
-        s.push(i.value);
-        i.value = "";
-        renderStack();
-      }
-    };
-    document.getElementById("popBtn").onclick = () => {
-      s.pop();
-      renderStack();
-    };
-    document.getElementById("enqueueBtn").onclick = () => {
-      const i = document.getElementById("queueInput");
-      if (i.value) {
-        q.enqueue(i.value);
-        i.value = "";
-        renderQueue();
-      }
-    };
-    document.getElementById("dequeueBtn").onclick = () => {
-      q.dequeue();
-      renderQueue();
-    };
-  }
-
-  // --- List ---
-  if (document.getElementById("appendBtn")) {
-    document.getElementById("appendBtn").onclick = () => {
-      const i = document.getElementById("llInput");
-      if (i.value) {
-        sl.append(i.value);
-        i.value = "";
-        renderLL();
-      }
-    };
-    document.getElementById("alAddBtn").onclick = () => {
-      const i = document.getElementById("alInput");
-      if (i.value) {
-        al.add(i.value);
-        i.value = "";
+  // --- [1. 동적 배열 - array.html] ---
+  if (document.getElementById("arrAddLastBtn")) {
+    const input = document.getElementById("arrInput"),
+      indexInput = document.getElementById("arrIndex");
+    document.getElementById("arrAddLastBtn").onclick = () => {
+      if (input.value) {
+        al.add(input.value);
+        input.value = "";
         renderAL();
       }
     };
+    document.getElementById("arrInsertBtn").onclick = () => {
+      const idx = parseInt(indexInput.value);
+      if (input.value && !isNaN(idx)) {
+        al.insertAt(idx, input.value);
+        renderAL();
+      }
+    };
+    document.getElementById("arrRemoveLastBtn").onclick = () => {
+      al.removeLast();
+      renderAL();
+    };
+    document.getElementById("arrGetBtn").onclick = () => {
+      const idx = parseInt(indexInput.value);
+      if (!isNaN(idx)) alert(`인덱스 ${idx}: ${al.get(idx)}`);
+    };
+    document.getElementById("arrSearchBtn").onclick = () => {
+      if (input.value) alert(`위치: ${al.indexOf(input.value)}`);
+    };
+    document.getElementById("arrSortBtn").onclick = () => {
+      al.sort();
+      renderAL();
+    };
+    document.getElementById("arrReverseBtn").onclick = () => {
+      al.reverse();
+      renderAL();
+    };
+    document.getElementById("arrClearBtn").onclick = () => {
+      al.clear();
+      renderAL();
+    };
+    renderAL();
   }
 
-  // --- Tree & Heap ---
-  if (document.getElementById("bstInsertBtn")) {
-    document.getElementById("bstInsertBtn").onclick = () => {
-      const i = document.getElementById("bstInput");
-      if (i.value) {
-        tr.insert(i.value);
-        i.value = "";
-        updateBST();
+  // --- [2. 연결 리스트 - list.html] ---
+  if (document.getElementById("listTypeSelect")) {
+    const typeSelect = document.getElementById("listTypeSelect"),
+      valInput = document.getElementById("listInput"),
+      idxInput = document.getElementById("listIndex");
+    const getActiveList = () =>
+      typeSelect.value === "singly"
+        ? sl
+        : typeSelect.value === "doubly"
+          ? dl
+          : cl;
+
+    const updateListUI = () => {
+      const type = typeSelect.value;
+      const arr = getActiveList().toArray ? getActiveList().toArray() : [];
+      document.getElementById("listSize").innerText = arr.length;
+
+      const cont = document.getElementById("list-visualizer");
+      if (!cont) return;
+      cont.style.display = "flex";
+      cont.style.flexWrap = "wrap";
+      cont.style.gap = "10px 0";
+
+      // 🌟 텍스트 대신 예쁜 블록(.list-node)으로 렌더링 🌟
+      if (type === "circular") {
+        cont.innerHTML = arr
+          .map(
+            (v, i) =>
+              `<div class="list-node" style="border-radius: 50%;">${v}</div>` +
+              (i < arr.length - 1
+                ? `<span class="list-arrow">➔</span>`
+                : `<span class="list-arrow">↻</span>`),
+          )
+          .join("");
+      } else {
+        const arrow = type === "doubly" ? "⇄" : "➔";
+        cont.innerHTML = arr
+          .map(
+            (v, i) =>
+              `<div class="list-node">${v}</div>` +
+              (i < arr.length - 1
+                ? `<span class="list-arrow">${arrow}</span>`
+                : ""),
+          )
+          .join("");
       }
     };
-    document.getElementById("bstDeleteBtn").onclick = () => {
-      const i = document.getElementById("bstInput");
-      if (i.value) {
-        tr.delete(i.value);
-        i.value = "";
-        updateBST();
+
+    typeSelect.onchange = updateListUI;
+    document.getElementById("listAddFrontBtn").onclick = () => {
+      if (valInput.value) {
+        getActiveList().prepend(valInput.value);
+        valInput.value = "";
+        updateListUI();
       }
     };
-    document.getElementById("avlInsertBtn").onclick = () => {
-      const i = document.getElementById("avlInput");
-      if (i.value) {
-        avl.insert(i.value);
-        i.value = "";
-        renderTree(avl.root, "avl-visualizer", "avl");
+    document.getElementById("listAddLastBtn").onclick = () => {
+      if (valInput.value) {
+        getActiveList().append(valInput.value);
+        valInput.value = "";
+        updateListUI();
       }
     };
-    document.getElementById("heapInsertBtn").onclick = () => {
-      const i = document.getElementById("heapInput");
-      if (i.value) {
-        hp.insert(i.value);
-        i.value = "";
-        updateHeap();
+    document.getElementById("listInsertBtn").onclick = () => {
+      const idx = parseInt(idxInput.value);
+      if (valInput.value && !isNaN(idx)) {
+        getActiveList().insertAt(idx, valInput.value);
+        updateListUI();
       }
     };
-    document.getElementById("heapExtractBtn").onclick = () => {
-      const max = hp.extractMax();
-      if (max !== null) alert("추출된 최댓값: " + max);
-      updateHeap();
+    document.getElementById("listRemoveBtn").onclick = () => {
+      const idx = parseInt(idxInput.value);
+      if (!isNaN(idx)) {
+        getActiveList().removeAt(idx);
+        updateListUI();
+      }
     };
+    document.getElementById("listSearchBtn").onclick = () => {
+      const idx = getActiveList().toArray().indexOf(valInput.value);
+      alert(idx !== -1 ? `위치: ${idx}` : "없음");
+    };
+    document.getElementById("listReverseBtn").onclick = () => {
+      getActiveList().reverse();
+      updateListUI();
+    };
+    updateListUI();
   }
 
-  // --- Graph ---
-  if (document.getElementById("addNodeBtn")) {
-    document.getElementById("addNodeBtn").onclick = () => {
-      const i = document.getElementById("nodeInput");
-      if (i.value) {
-        g.addVertex(i.value);
-        const cont = document.getElementById("graph-visualizer");
-        nodePositions[i.value] = {
-          x: Math.random() * (cont.clientWidth - 80) + 40,
-          y: Math.random() * (cont.clientHeight - 80) + 40,
-        };
-        i.value = "";
-        renderGraph();
+  // --- [3. 트리 구조 - tree.html] ---
+  if (document.getElementById("treeTypeSelect")) {
+    const treeType = document.getElementById("treeTypeSelect"),
+      input = document.getElementById("treeInput");
+
+    document.getElementById("treeInsertBtn").onclick = () => {
+      if (!input.value) return;
+      const val = parseInt(input.value);
+      if (treeType.value === "bst") tr.insert(val);
+      else if (treeType.value === "avl") avl.insert(val);
+      else hp.insert(val);
+      input.value = "";
+      updateTreeUI();
+    };
+    document.getElementById("treeDeleteBtn").onclick = () => {
+      if (!input.value) return;
+      if (treeType.value === "bst") tr.delete(parseInt(input.value));
+      input.value = "";
+      updateTreeUI();
+    };
+    document.getElementById("treeClearBtn").onclick = () => {
+      if (treeType.value === "bst") tr.root = null;
+      else if (treeType.value === "avl") avl.root = null;
+      else hp.heap = [];
+      updateTreeUI();
+    };
+    treeType.onchange = updateTreeUI;
+    updateTreeUI();
+  }
+
+  // --- [4. 스택/큐/데크 - linear.html] ---
+  if (document.getElementById("linearTypeSelect")) {
+    const typeSelect = document.getElementById("linearTypeSelect"),
+      input = document.getElementById("linearInput");
+    const getActiveLinear = () =>
+      typeSelect.value === "stack" ? s : typeSelect.value === "queue" ? q : dq;
+
+    const updateLinearUI = () => {
+      const type = typeSelect.value;
+      const arr = getActiveLinear().toArray();
+      const cont = document.getElementById("linear-visualizer");
+
+      document.getElementById("linearSizeLabel").innerText = arr.length;
+      document.getElementById("linearTopLabel").innerText =
+        arr.length > 0
+          ? type === "stack"
+            ? arr[arr.length - 1]
+            : arr[0]
+          : "Empty";
+
+      cont.innerHTML = "";
+      cont.style.flexDirection = type === "stack" ? "column-reverse" : "row"; // 스택은 세로, 큐는 가로
+
+      arr.forEach((v, i) => {
+        const wrapper = document.createElement("div");
+        wrapper.style.display = "flex";
+        wrapper.style.flexDirection = "column";
+        wrapper.style.alignItems = "center";
+        if (type === "stack" && i === arr.length - 1) {
+          const ind = document.createElement("div");
+          ind.className = "top-indicator";
+          ind.innerText = "↑ Top";
+          wrapper.appendChild(ind);
+        }
+        const node = document.createElement("div");
+        node.className = "linear-node";
+        node.innerText = v;
+        wrapper.appendChild(node);
+        cont.appendChild(wrapper);
+      });
+    };
+
+    typeSelect.onchange = updateLinearUI;
+    document.getElementById("linearPushBtn").onclick = () => {
+      if (input.value) {
+        getActiveLinear().push
+          ? getActiveLinear().push(input.value)
+          : getActiveLinear().enqueue
+            ? getActiveLinear().enqueue(input.value)
+            : getActiveLinear().addBack(input.value);
+        input.value = "";
+        updateLinearUI();
       }
     };
-    document.getElementById("addEdgeBtn").onclick = () => {
-      const f = document.getElementById("fromNode").value,
-        t = document.getElementById("toNode").value;
-      if (f && t) {
-        g.addEdge(f, t);
-        renderGraph();
-      }
+    document.getElementById("linearPopBtn").onclick = () => {
+      getActiveLinear().pop
+        ? getActiveLinear().pop()
+        : getActiveLinear().dequeue
+          ? getActiveLinear().dequeue()
+          : getActiveLinear().removeFront();
+      updateLinearUI();
     };
+    document.getElementById("linearSizeBtn").onclick = () =>
+      alert(`크기: ${getActiveLinear().toArray().length}`);
+    document.getElementById("linearSearchBtn").onclick = () => {
+      const idx = getActiveLinear().toArray().indexOf(input.value);
+      alert(idx !== -1 ? `위치: ${idx}` : "없음");
+    };
+    document.getElementById("linearClearBtn").onclick = () => {
+      while (getActiveLinear().toArray().length > 0) {
+        getActiveLinear().pop
+          ? getActiveLinear().pop()
+          : getActiveLinear().dequeue
+            ? getActiveLinear().dequeue()
+            : getActiveLinear().removeFront();
+      }
+      updateLinearUI();
+    };
+    updateLinearUI();
   }
 };
 
-// --- [도우미 함수] ---
-function updateBST() {
-  renderTree(tr.root, "bst-visualizer", "bst");
-  const inorder = [];
-  const traverse = (n) => {
-    if (!n) return;
-    traverse(n.left);
-    inorder.push(n.val);
-    traverse(n.right);
-  };
-  traverse(tr.root);
-  const res = document.getElementById("bst-inorder-result");
-  if (res) res.innerText = "중위 순회(정렬 결과): " + inorder.join(" → ");
-}
-
-function updateHeap() {
-  renderTree(hp.toTree(), "heap-visualizer", "heap");
-  const arrayVis = document.getElementById("heap-array-visualizer");
-  if (arrayVis)
-    arrayVis.innerHTML = hp.heap
-      .map(
-        (v, i) =>
-          `<div class="array-cell"><div class="index-label">${i}</div><div class="node">${v}</div></div>`,
-      )
-      .join("");
-}
-
-// --- [공통 렌더링] ---
-function renderStack() {
-  document.getElementById("stack-visualizer").innerHTML = s
-    .toArray()
-    .map((v) => `<div class="node">${v}</div>`)
-    .join("");
-}
-function renderQueue() {
-  document.getElementById("queue-visualizer").innerHTML = q
-    .toArray()
-    .map((v) => `<div class="node">${v}</div>`)
-    .join("");
-}
-function renderLL() {
-  const a = sl.toArray();
-  document.getElementById("ll-visualizer").innerHTML = a
-    .map(
-      (v, i) =>
-        `<div class="node">${v}</div>${i < a.length - 1 ? '<span class="ll-arrow">➔</span>' : ""}`,
-    )
-    .join("");
-}
+// --- [렌더링 함수 그룹] ---
 function renderAL() {
-  const cont = document.getElementById("al-visualizer");
-  if (cont)
-    cont.innerHTML = al
-      .toArray()
-      .map(
-        (v, i) =>
-          `<div class="array-cell"><div class="index-label">${i}</div><div class="node">${v}</div></div>`,
-      )
-      .join("");
+  const cont = document.getElementById("array-visualizer");
+  if (!cont) return;
+  cont.innerHTML = al
+    .toArray()
+    .map((v, i) => {
+      const isVal = v !== undefined;
+      return `<div class="array-cell" style="margin-right: 2px;">
+                <div class="index-label" style="color: #64748b; text-align: center; margin-bottom: 5px;">${i}</div>
+                <div class="node" style="width: 50px; height: 50px; display: flex; align-items: center; justify-content: center; font-weight: bold; border-radius: 4px; border: 2px solid ${isVal ? "#3b82f6" : "#cbd5e1"}; background-color: ${isVal ? "#eff6ff" : "#f8fafc"}; color: ${isVal ? "#1e3a8a" : "transparent"};">${isVal ? v : ""}</div>
+            </div>`;
+    })
+    .join("");
+  if (document.getElementById("arrSize"))
+    document.getElementById("arrSize").innerText = al.size;
+  if (document.getElementById("arrCapacity"))
+    document.getElementById("arrCapacity").innerText = al.capacity;
 }
 
+function updateTreeUI() {
+  const type = document.getElementById("treeTypeSelect").value;
+  const arrayVis = document.getElementById("heap-array-visualizer");
+  if (type === "heap") {
+    if (arrayVis) {
+      arrayVis.style.display = "flex";
+      arrayVis.innerHTML = hp.heap
+        .map(
+          (v, i) =>
+            `<div style="margin:5px; text-align:center;"><div>${i}</div><div style="width:40px;height:40px;background:#10b981;color:white;display:flex;align-items:center;justify-content:center;border-radius:4px;font-weight:bold;">${v}</div></div>`,
+        )
+        .join("");
+    }
+    renderTree(hp.toTree(), "tree-visualizer", "heap");
+  } else {
+    if (arrayVis) arrayVis.style.display = "none";
+    renderTree(type === "bst" ? tr.root : avl.root, "tree-visualizer", type);
+  }
+}
+
+// 🌟 트리를 화면에 그리는 핵심 로직 복구 완료 🌟
 function renderTree(root, containerId, prefix, highlightNode = null) {
   const cont = document.getElementById(containerId);
   if (!cont) return;
@@ -215,13 +363,8 @@ function renderTree(root, containerId, prefix, highlightNode = null) {
     const wrapper = document.createElement("div");
     wrapper.className = "tree-wrapper";
     const div = document.createElement("div");
-    div.className = "node bst-node";
-    if (prefix === "avl") {
-      const bf = avl.getBalance(node);
-      div.innerHTML = `${node.val}<span style="font-size:10px; color:#d93025; position:absolute; top:-18px;">BF:${bf}</span>`;
-    } else {
-      div.innerText = node.val;
-    }
+    div.className = "bst-node";
+    div.innerText = node.val;
     const uId = node.idx !== undefined ? node.idx : node.val;
     div.id = `${prefix}-node-${uId}`;
     wrapper.appendChild(div);
@@ -273,8 +416,8 @@ function renderTree(root, containerId, prefix, highlightNode = null) {
             line.setAttribute("y1", pY);
             line.setAttribute("x2", cX);
             line.setAttribute("y2", cY);
-            line.setAttribute("stroke", "#9c27b0");
-            line.setAttribute("stroke-width", "2");
+            line.setAttribute("stroke", "#10b981");
+            line.setAttribute("stroke-width", "3");
             svg.appendChild(line);
             draw(child);
           }
@@ -283,41 +426,4 @@ function renderTree(root, containerId, prefix, highlightNode = null) {
       draw(root);
     });
   }
-}
-
-function renderGraph() {
-  const cont = document.getElementById("graph-visualizer");
-  if (!cont) return;
-  cont.innerHTML = "";
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.setAttribute("class", "tree-svg-layer");
-  svg.setAttribute("width", "100%");
-  svg.setAttribute("height", "100%");
-  cont.appendChild(svg);
-  g.getEdges().forEach(([v1, v2]) => {
-    const p1 = nodePositions[v1],
-      p2 = nodePositions[v2];
-    if (p1 && p2) {
-      const line = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "line",
-      );
-      line.setAttribute("x1", p1.x);
-      line.setAttribute("y1", p1.y);
-      line.setAttribute("x2", p2.x);
-      line.setAttribute("y2", p2.y);
-      line.setAttribute("stroke", "#1a73e8");
-      line.setAttribute("stroke-width", "2");
-      svg.appendChild(line);
-    }
-  });
-  g.getVertices().forEach((v) => {
-    const div = document.createElement("div");
-    div.className = "node graph-node";
-    div.style.left = `${nodePositions[v].x}px`;
-    div.style.top = `${nodePositions[v].y}px`;
-    div.style.transform = "translate(-50%, -50%)";
-    div.innerText = v;
-    cont.appendChild(div);
-  });
 }
--- js/models/ArrayList.js
+++ js/models/ArrayList.js
@@ -1,28 +1,88 @@
 export class ArrayList {
   constructor() {
-    this.items = [];
+    this.capacity = 4; // 초기 용량
+    this.size = 0; // 실제 데이터 개수
+    this.array = new Array(this.capacity);
   }
 
-  get(index) {
-    return this.items[index];
+  // 용량이 꽉 찼을 때 2배로 늘리는 내부 메서드
+  _resize() {
+    this.capacity *= 2;
+    const newArray = new Array(this.capacity);
+    for (let i = 0; i < this.size; i++) {
+      newArray[i] = this.array[i];
+    }
+    this.array = newArray;
   }
 
   add(val) {
-    this.items.push(val);
+    if (this.size === this.capacity) this._resize();
+    this.array[this.size] = val;
+    this.size++;
   }
 
   insertAt(index, val) {
-    this.items.splice(index, 0, val);
-  }
-
-  removeAt(index) {
-    this.items.splice(index, 1);
+    if (index < 0 || index > this.size) return false;
+    if (this.size === this.capacity) this._resize();
+    for (let i = this.size; i > index; i--) {
+      this.array[i] = this.array[i - 1];
+    }
+    this.array[index] = val;
+    this.size++;
+    return true;
   }
 
   removeLast() {
-    return this.items.pop();
+    if (this.size === 0) return null;
+    const val = this.array[this.size - 1];
+    this.array[this.size - 1] = undefined; // 메모리 해제 시뮬레이션
+    this.size--;
+    return val;
   }
+
+  get(index) {
+    if (index < 0 || index >= this.size) return null;
+    return this.array[index];
+  }
+
+  indexOf(val) {
+    for (let i = 0; i < this.size; i++) {
+      // 입력값이 문자열일 수 있으므로 동등 연산자 사용
+      if (this.array[i] == val) return i;
+    }
+    return -1;
+  }
+
+  sort() {
+    // 유효한 데이터만 잘라서 정렬 후 원본에 덮어쓰기
+    const validData = this.array.slice(0, this.size);
+    validData.sort((a, b) => {
+      if (!isNaN(a) && !isNaN(b)) return Number(a) - Number(b);
+      return String(a).localeCompare(String(b));
+    });
+    for (let i = 0; i < this.size; i++) this.array[i] = validData[i];
+  }
+
+  reverse() {
+    let left = 0;
+    let right = this.size - 1;
+    while (left < right) {
+      let temp = this.array[left];
+      this.array[left] = this.array[right];
+      this.array[right] = temp;
+      left++;
+      right--;
+    }
+  }
+
+  clear() {
+    this.capacity = 4;
+    this.size = 0;
+    this.array = new Array(this.capacity);
+  }
+
   toArray() {
-    return [...this.items];
+    // UI에 빈 공간(Capacity 여분)도 보여주기 위해 전체 배열 반환
+    return this.array;
   }
 }
